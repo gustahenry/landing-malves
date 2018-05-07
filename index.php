ecd1569114a148a9d469b46076b1429566a50dc8
gustavoDesen <pcasemirog@gmail.com>
2018-05-07 14:52:29 -0300

    inclusao do site

---
+++ index.php
@@ -0,0 +1,180 @@
+<?php require_once("header.php"); ?>
+
+<div class="above">
+  <div class="container">
+    <div class="row justify-content-between">
+      <div class="col-lg-6 above-content">
+        <h2 class="font-weight-bold">Lorem Ipsium Dolor<br> sit amet lorem</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident cum repudiandae quos consectetur delectus fugit quam deserunt vero in, eaque illum sit, non libero! Et sequi sit labore recusandae porro!</p>
+
+        <ul class="above-itens text-right">
+          <li>Lorem ipsum dolor sit amet, consectetur</li>
+          <li>Lorem ipsum dolor sit amet, consectetur</li>
+          <li>Lorem ipsum dolor sit amet, consectetur</li>
+          <li>Lorem ipsum dolor sit amet, consectetur</li>
+        </ul>
+      </div>
+      <div class="col-lg-4">
+        <p class="text-center font-weight-bold">Lorem Ipsium Dolor sit amet lorem</p>
+        <div class="form-landing"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="action">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10">
+        <h3>Lorem Ipsium Dolor sit amet lorem Ipsium Dolor sit amet lorem</h3>
+        <p>Lorem Ipsium Dolor sit amet Lorem Ipsium Dolor sit amet Lorem Ipsium Dolor sit amet </p>
+      </div>
+      <div class="col-lg-2 text-right">
+        <a class="btn btn-custom btn-lg" href="#" role="button">Saiba Mais</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="information-1">
+  <div class="container">
+    <h2 class="text-center information-title"><span>67% </span>Lorem Ipsium Dolor sit amet lorem</h2>
+    <div class="row">
+      <div class="col-lg-4">
+        <div style="background-color: #444; width: 100%; height: 300px; border-radius: 15px;"></div>
+      </div>
+      <div class="col-lg-4">
+        <div style="background-color: #444; width: 100%; height: 300px; border-radius: 15px;"></div>
+      </div>
+      <div class="col-lg-4">
+        <div style="background-color: #444; width: 100%; height: 300px; border-radius: 15px;"></div>
+      </div>
+    </div>
+    <div class="text-center mt-5">
+      <p class="font-weight-bold">Lorem Ipsium Dolor sit amet Lorem Ipsium Dolor sit amet <br>Lorem Ipsium Dolor sit amet </p>
+      <a class="btn btn-custom btn-lg" href="#" role="button">Saiba Mais</a>
+    </div>
+  </div>
+</div>
+<div class="information-2">
+  <div class="container">
+    <h2 class="text-center information-title">Lorem Ipsium Dolor sit amet lorem</h2>
+    <div class="row">
+      <div class="col-lg-3">
+        <div style="background-color: #444; width: 100%; height: 250px; border-radius: 0px;"></div>
+      </div>
+      <div class="col-lg-3">
+        <div style="background-color: #444; width: 100%; height: 250px; border-radius: 0px;"></div>
+      </div>
+      <div class="col-lg-3">
+        <div style="background-color: #444; width: 100%; height: 250px; border-radius: 0px;"></div>
+      </div>
+      <div class="col-lg-3">
+        <div style="background-color: #444; width: 100%; height: 250px; border-radius: 0px;"></div>
+      </div>
+    </div>
+    <div class="text-center mt-5">
+      <a class="btn btn-custom btn-lg" href="#" role="button">Saiba Mais</a>
+    </div>
+  </div>
+</div>
+<div class="information-3">
+  <div class="container">
+    <h2 class="text-center information-title">Lorem Ipsium Dolor sit amet lorem</h2>
+    <div class="row">
+      <div class="col-lg-4">
+        <div style="background-color: #444; width: 80%; height: 250px; border-radius: 15px; margin-top: 10%; margin-bottom: 10%;" class="mx-auto"></div>
+      </div>
+      <div class="col-lg-4">
+        <div style="background-color: #444; width: 100%; height: 300px; border-radius: 15px;"></div>
+      </div>
+      <div class="col-lg-4">
+        <div style="background-color: #444; width: 80%; height: 250px; border-radius: 15px; margin-top: 10%; margin-bottom: 10%;" class="mx-auto"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="information-4">
+  <div class="container">
+    <div class="text-center information-title">
+      <h2>Lorem Ipsium Dolor sit amet lorem</h2>
+      <p>Lorem Ipsium Dolor sit amet lorem</p>
+    </div>
+    <div class="row">
+      <div class="col-lg-3">
+        <div style="background-color: #444; width: 80%; height: 250px; border-radius: 0px; margin-top: 10%; margin-bottom: 10%;" class="mx-auto"></div>
+      </div>
+      <div class="col-lg-3">
+        <div style="background-color: #444; width: 100%; height: 300px; border-radius: 0px;"></div>
+      </div>
+      <div class="col-lg-3">
+        <div style="background-color: #444; width: 80%; height: 250px; border-radius: 0px; margin-top: 10%; margin-bottom: 10%;" class="mx-auto"></div>
+      </div>
+      <div class="col-lg-3">
+        <div style="background-color: #444; width: 80%; height: 250px; border-radius: 0px; margin-top: 10%; margin-bottom: 10%;" class="mx-auto"></div>
+      </div>
+    </div>
+    <div class="text-center mt-5">
+      <a class="btn btn-custom btn-lg" href="#" role="button">Saiba Mais</a>
+    </div>
+  </div>
+</div>
+<div class="information-5">
+  <div class="container">
+       <h4 class="text-center information-subtitle">Lorem Ipsium Dolor sit amet lorem</h4>
+      <div class="row justify-content-center">
+        <div class="col-lg-2">
+          <div style="background-color: #444; width: 100%; height: 50px; border-radius: 0px;"></div>
+        </div>
+        <div class="col-lg-2">
+          <div style="background-color: #444; width: 100%; height: 50px; border-radius: 0px;"></div>
+        </div>
+        <div class="col-lg-2">
+          <div style="background-color: #444; width: 100%; height: 50px; border-radius: 0px;"></div>
+        </div>
+        <div class="col-lg-2">
+          <div style="background-color: #444; width: 100%; height: 50px; border-radius: 0px;"></div>
+        </div>
+      </div>
+  </div>
+</div>
+<div class="information-6">
+  <div class="container">
+    <div class="text-center information-title">
+      <h2>Lorem Ipsium Dolor sit amet lorem</h2>
+      <p>Lorem Ipsium Dolor sit amet lorem</p>
+    </div>
+    <div class="text-center">
+      <p>Lorem Ipsium Dolor sit amet lorem Lorem Ipsium Dolor sit amet lore?</p>
+      <p>Lorem Ipsium Dolor sit amet lorem Lorem Ipsium Dolor sit amet lore?</p>
+      <p>Lorem Ipsium Dolor sit amet lorem Lorem Ipsium Dolor sit amet lore?</p>
+      <p>Lorem Ipsium Dolor sit amet lorem Lorem Ipsium Dolor sit amet lore?</p>
+      <p>Lorem Ipsium Dolor sit amet lorem Lorem Ipsium Dolor sit amet lore?</p>
+    </div>
+  </div>
+</div>
+<div class="action">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10">
+        <h3>Lorem Ipsium Dolor sit amet lorem Ipsium Dolor sit amet lorem</h3>
+        <p>Lorem Ipsium Dolor sit amet Lorem Ipsium Dolor sit amet Lorem Ipsium Dolor sit amet </p>
+      </div>
+      <div class="col-lg-2 text-right">
+        <a class="btn btn-custom btn-lg" href="#" role="button">Saiba Mais</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="footer">
+  <div class="container">
+     <div class="row">
+      <div class="col-lg-10">
+        <p>2018 - Malves Estúdio Criativo - Todos os direitos reservados.</p>
+      </div>
+      <div class="col-lg-2 text-right">
+        
+      </div>
+    </div>
+  </div>
+</div>
+<?php require_once("footer.php"); ?>
